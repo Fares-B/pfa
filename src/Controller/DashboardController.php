be75fa5d318a2efe6ec88b36d2a8a54932b44659
Fares.B <f_bourih@etu-webschoolfactory.fr>
2022-03-14 17:56:50 +0100

    auth and configure tailwindcss

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->redirectToRoute("app_dashboard");
+    }
+    /**
+     * @Route("/dashboard", name="app_dashboard")
+     */
+    public function dashboard(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
